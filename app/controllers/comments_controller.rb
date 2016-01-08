f54e372123d5047c69091945d9095d5bacea9adf
HatemKhattab <hattemkhattab@gmail.com>
2016-01-08 23:17:04 +0000

    add_comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,31 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
+  def create
+    @link = Link.find(params[:link_id])
+    @comment = @link.comments.new(comment_params)
+    @comment.user = current_user
+
+      if @comment.save
+        redirect_to link_path(@link), notice: 'Comment was successfully created.'
+      else
+        render :new
+      end
+  end
+
+
+  def destroy
+    @comment.destroy
+    redirect_to :back, notice: 'Comment was successfully destroyed.'
+  end
+
+  private
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:link_id, :body, :user_id)
+    end
+end
