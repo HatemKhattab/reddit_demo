f7d26aff619706cbb5cf8bce764a007a3026f950
HatemKhattab <hattemkhattab@gmail.com>
2016-01-09 00:55:39 +0000

    add name to User

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -19,14 +19,10 @@ class LinksController < ApplicationController
 
   def create
     @link = current_user.links.build(link_params)
-    respond_to do |format|
       if @link.save
-        format.html { redirect_to @link, notice: 'Link was successfully created.' }
-        format.json { render :show, status: :created, location: @link }
+        redirect_to root_path, notice: 'Link was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
+         render :new
     end
   end
 
